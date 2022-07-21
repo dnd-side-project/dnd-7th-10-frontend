92fa50aef7b2c5a72798fc6c70443c3e54bb2ce6
esllo <master@esllo.com>
2022-07-21 16:35:22 +0900

    [#13] feat: add Webview and TextInput to App.tsx

--- App.tsx
+++ App.tsx
@@ -8,113 +8,61 @@
  * @format
  */
 
-import React, { type PropsWithChildren } from 'react'
+import React from 'react'
 import {
+  KeyboardAvoidingView,
+  Platform,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
+  TextInput,
   useColorScheme,
   View
 } from 'react-native'
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions
-} from 'react-native/Libraries/NewAppScreen'
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string
-  }>
-> = ({ children, title }) => {
-  const isDarkMode = useColorScheme() === 'dark'
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black
-          }
-        ]}
-      >
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark
-          }
-        ]}
-      >
-        {children}
-      </Text>
-    </View>
-  )
-}
+import WebView from 'react-native-webview'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark'
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1
   }
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
       >
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white
-          }}
-        >
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        <View style={styles.view}>
+          <WebView
+            originWhitelist={['*']}
+            source={{ uri: 'https://naver.com' }}
+            style={styles.webview}
+          />
         </View>
-      </ScrollView>
+        <TextInput style={styles.textInput} multiline editable />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
+  container: {
+    flex: 1
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600'
+  view: {
+    flex: 1,
+    minHeight: 300
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400'
+  webview: {
+    flex: 1
   },
-  highlight: {
-    fontWeight: '700'
+  textInput: {
+    minHeight: 100
   }
 })
 

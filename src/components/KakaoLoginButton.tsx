b4e69fcde6e7fe654e24185f8d24e3791c3599cc
esllo <master@esllo.com>
2022-08-10 01:46:57 +0900

    [#26] feat: add kakao login button

---
+++ src/components/KakaoLoginButton.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from '@emotion/native'
+
+const KakaoTouch = styled.TouchableOpacity`
+  width: 100%;
+`
+
+const KakaoButton = styled.View`
+  flex-direction: row;
+  border-radius: 4px;
+  background: #fee500;
+  height: 56px;
+  align-items: center;
+  justify-content: center;
+`
+
+const KakaoText = styled.Text`
+  font-family: 'Pretendard-SemiBold';
+  font-size: 15px;
+  color: #181600;
+`
+
+const KakaoIcon = styled.Image`
+  width: 24px;
+  height: 23px;
+`
+
+const KakaoLoginButton = () => {
+  return (
+    <KakaoTouch activeOpacity={0.8}>
+      <KakaoButton>
+        <KakaoIcon
+          source={require('../assets/images/kakao_login_large_wide.png')}
+        />
+        <KakaoText>카카오로 시작하기</KakaoText>
+      </KakaoButton>
+    </KakaoTouch>
+  )
+}
+
+export default KakaoLoginButton

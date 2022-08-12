b1f5caeda90287e0190c8b0c19e7e163335543ac
nicky0830 <kwaknicky@naver.com>
2022-08-13 00:58:05 +0900

    [#16] fix: header component fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,18 @@
 import React from 'react'
 import styled from '@emotion/native'
-import { Image } from 'react-native'
 
 const HeaderBar = styled.View`
   flex-direction: row;
   justify-content: space-between;
-  margin-top: 50px;
-  padding: 0px 10px 10px 10px;
+  margin-top: 40px;
+  padding: 10px;
 `
-const HeaderTitle = styled.Text`
-  text-align: left;
-  flex: 1;
-  margin-left: 20px;
-`
+// const HeaderTitle = styled.Text`
+//     text-align: left;
+//     flex: 1;
+//     margin-left: 20px;
+// `
 
-const Header = ({ text }) => (
-  <HeaderBar>
-    <Image source={require('../assets/images/backarrow.png')} />
-    <HeaderTitle>{text}</HeaderTitle>
-  </HeaderBar>
-)
+const Header = () => <HeaderBar />
 
 export default Header

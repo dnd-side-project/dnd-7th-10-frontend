aae32a9ca42350ec0e89e9cf6b8aa562c61cbd5b
Seokhwan Park <master@esllo.com>
2022-08-13 23:02:10 +0900

    [#28] 폴더 콘텐츠 (#55)
    * [#28] chore: add image assets
    
    * [#28] feat: add folder card
    
    * [#28] feat: add folder card list
    
    * [#28] feat: add folder content page

---
+++ src/pages/FolderContent.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from '@emotion/native'
+import FolderCardList from '../components/FolderContent/FolderCardList'
+import Header, { IIconButton } from '../components/Header'
+
+const FolderDescView = styled.View`
+  flex: 1;
+`
+
+const FolderContent = () => {
+  const iconButtons: IIconButton[] = [
+    {
+      name: 'search',
+      source: require('../assets/images/search.png'),
+      onPress: () => console.log('h')
+    },
+    {
+      name: 'link',
+      source: require('../assets/images/link.png'),
+      onPress: () => console.log('h')
+    }
+  ]
+
+  return (
+    <FolderDescView>
+      <Header iconButtons={iconButtons}>디자인 레퍼런스</Header>
+      <FolderCardList />
+    </FolderDescView>
+  )
+}
+
+export default FolderContent

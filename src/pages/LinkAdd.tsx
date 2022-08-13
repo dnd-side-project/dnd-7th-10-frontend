16a473d5d3fee5ac47925e35c695f5a1f9c725e3
esllo <master@esllo.com>
2022-08-14 01:22:22 +0900

    [#60] feat: add link_add page

---
+++ src/pages/LinkAdd.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from '@emotion/native'
+import Header from '../components/Header'
+import Input from '../components/Input'
+import { backgroundWithColor } from '../styles/backgrounds'
+import SectionTitle from '../components/SectionTitle'
+import SectionContent from '../components/SectionContent'
+import FolderSelectList from '../components/LinkAdd/FolderSelectList'
+import TagGuide from '../components/LinkAdd/TagGuide'
+import TagList from '../components/TagList'
+
+const LinkAddPageView = styled.View`
+  ${backgroundWithColor('gray_1')}
+  flex: 1;
+`
+
+const LinkContentScroll = styled.ScrollView`
+  flex: 1;
+`
+
+const LinkAddContentView = styled.View`
+  ${backgroundWithColor('gray_1')}
+  padding: 0 24px;
+  flex: 1;
+`
+
+const LinkAddPage = () => {
+  return (
+    <LinkAddPageView>
+      <Header>링크추가</Header>
+      <LinkContentScroll>
+        <LinkAddContentView>
+          <SectionTitle title="링크 URL" />
+          <SectionContent>
+            <Input />
+          </SectionContent>
+          <SectionTitle title="저장할 폴더 선택" plus />
+          <SectionContent>
+            <FolderSelectList />
+          </SectionContent>
+          <SectionTitle title="태그 선택" plus />
+          <SectionContent>
+            <TagGuide />
+            <TagList />
+          </SectionContent>
+        </LinkAddContentView>
+      </LinkContentScroll>
+    </LinkAddPageView>
+  )
+}
+
+export default LinkAddPage

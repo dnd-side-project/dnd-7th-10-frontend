86dfa774e51309bb28ed088ea2a70cbca4d2b326
esllo <master@esllo.com>
2022-08-14 01:20:44 +0900

    [#60] feat: add common/section_title component

---
+++ src/components/SectionTitle.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from '@emotion/native'
+import { fontWithColorFamily } from '../styles/fonts'
+import { flexWithAlign } from '../styles/flexbox'
+
+const SectionTitleView = styled.View`
+  ${flexWithAlign('center', 'space-between', 'row')}
+  margin-top: 24px;
+  margin-bottom: 20px;
+`
+
+const SectionTitleText = styled.Text`
+  ${fontWithColorFamily('gray_7', 'SemiBold')}
+  font-size: 18px;
+`
+
+const SectionTitleIcon = styled.Image`
+  width: 24px;
+  height: 24px;
+`
+
+interface Props {
+  title: string
+  plus?: boolean
+}
+
+const SectionTitle = ({ title, plus }: Props) => {
+  return (
+    <SectionTitleView>
+      <SectionTitleText>{title}</SectionTitleText>
+      {plus && (
+        <SectionTitleIcon
+          source={require('../assets/images/plus.png')}
+          resizeMode="contain"
+        />
+      )}
+    </SectionTitleView>
+  )
+}
+
+export default SectionTitle

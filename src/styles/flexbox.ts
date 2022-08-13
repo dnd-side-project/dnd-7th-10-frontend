daf0da842525a51ea19925e6a1461c121898d6a4
esllo <master@esllo.com>
2022-08-13 18:13:08 +0900

    [#49] refactor: fix justify type, add flex direction

--- src/styles/flexbox.ts
+++ src/styles/flexbox.ts
@@ -1,19 +1,37 @@
 import { css } from '@emotion/native'
 import { FlexAlignType } from 'react-native'
 
+export type FlexJustifyType =
+  | 'flex-start'
+  | 'flex-end'
+  | 'center'
+  | 'space-between'
+  | 'space-around'
+  | 'space-evenly'
+  | undefined
+
+export type FlexDirection =
+  | 'row'
+  | 'column'
+  | 'row-reverse'
+  | 'column-reverse'
+  | undefined
+
 function flexWithAlign(
   alignItems: FlexAlignType = 'stretch',
-  justifyContent: FlexAlignType = 'flex-start'
+  justifyContent: FlexJustifyType = 'flex-start',
+  flexDirection: FlexDirection = 'column'
 ) {
   return css`
     align-items: ${alignItems};
     justify-content: ${justifyContent};
+    flex-direction: ${flexDirection};
   `
 }
 
 function flexWithSelfAlign(
   alignItems: FlexAlignType = 'stretch',
-  justifyContent: FlexAlignType = 'flex-start'
+  justifyContent: FlexJustifyType = 'flex-start'
 ) {
   return css`
     align-self: ${alignItems};

6437008f4f1507254989b4adc9fca8ad7ca93cd9
nicky0830 <kwaknicky@naver.com>
2022-08-14 12:30:17 +0900

    [#16] feat: add memocontent, memo, memotop

---
+++ src/components/MemoContent.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from '@emotion/native'
+import MemoTop from '../components/MemoTop'
+import Memo from './Memo'
+
+const MemoContentView = styled.View`
+  padding: 24px;
+`
+
+const MemoContainer = styled.View`
+  flex-direction: column;
+  justify-content: center;
+`
+
+const MemoContent = () => {
+  return (
+    <MemoContentView>
+      <MemoTop />
+      <MemoContainer>
+        <Memo content=" Read the docs to discover what to do next:e" />
+        <Memo content=" Read the docs to discover what to do next:e" />
+        <Memo content=" Read the docs to discover what to do next:e" />
+      </MemoContainer>
+    </MemoContentView>
+  )
+}
+
+export default MemoContent

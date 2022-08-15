2377aa9cb59a1b640b2ea95364e109205b473963
esllo <master@esllo.com>
2022-08-16 01:37:31 +0900

    [#75] feat: add svg object

---
+++ src/assets/images/svg.ts
@@ -0,0 +1,25 @@
+import Bell from './bell.svg'
+import Check from './check.svg'
+import ChevronLeft from './chevron-left.svg'
+import Edit from './edit.svg'
+import Link from './link.svg'
+import Member from './member.svg'
+import Memo from './memo.svg'
+import Remove from './remove.svg'
+import Search from './search.svg'
+import Warning from './warning.svg'
+
+const SVG = {
+  Bell,
+  Check,
+  ChevronLeft,
+  Edit,
+  Link,
+  Member,
+  Memo,
+  Remove,
+  Search,
+  Warning
+}
+
+export default SVG

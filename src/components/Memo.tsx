d6efde688d6cec5f6e0aa9d7f94f9a567afc3c83
esllo <master@esllo.com>
2022-08-15 20:40:47 +0900

    [#16] refactor: add types for memo

--- src/components/Memo.tsx
+++ src/components/Memo.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import styled from '@emotion/native'
 import { ColorPalette, FontFamily } from '../styles/variable'
-import { Image } from 'react-native'
 
 const MemoContent = styled.View`
   padding: 12px;
   height: 182px;
-  background-color: ${ColorPalette['background_1']};
+  background-color: ${ColorPalette.background_1};
   border-radius: 5px;
-  border: 1px solid ${ColorPalette['gray_4']};
+  border: 1px solid ${ColorPalette.gray_4};
 `
 
 const TopView = styled.View`
@@ -17,27 +16,32 @@ const TopView = styled.View`
   margin-bottom: 16px;
 `
 const MemoDate = styled.Text`
-  color: ${ColorPalette['gray_4']};
+  color: ${ColorPalette.gray_4};
 `
 const MemoText = styled.Text`
-  font-family: ${FontFamily['Regular']};
+  font-family: ${FontFamily.Regular};
   font-style: normal;
   font-weight: 400;
   font-size: 16px;
   line-height: 19px;
   display: flex;
   align-items: center;
-  color: ${ColorPalette['gray_6']};
+  color: ${ColorPalette.gray_6};
 `
 
-const Memo = ({ content }) => {
+const MemoImage = styled.Image`
+  width: 24px;
+  height: 24px;
+`
+interface Props {
+  content: string
+}
+
+const Memo = ({ content }: Props) => {
   return (
     <MemoContent>
       <TopView>
-        <Image
-          source={require('../assets/images/icon_memo.png')}
-          style={{ width: 24, height: 24 }}
-        />
+        <MemoImage source={require('../assets/images/icon_memo.png')} />
         <MemoDate>2022.08.13</MemoDate>
       </TopView>
       <MemoText>{content}</MemoText>

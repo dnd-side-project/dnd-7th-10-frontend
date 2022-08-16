62a1591b5ced7e604f2d7635fd8f807042be3a6f
esllo <master@esllo.com>
2022-08-17 03:50:13 +0900

    [#82] feat: add scroll_view to folder_add

--- src/pages/FolderAdd.tsx
+++ src/pages/FolderAdd.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import styled from '@emotion/native'
 import Header from '../components/Common/Header'
 import SectionTitle from '../components/Common/SectionTitle'
@@ -13,6 +13,11 @@ import useFolderList from '../components/Home/FolderList.hook'
 import { useNavigation } from '@react-navigation/native'
 
 const FolderAddView = styled.View`
+  flex: 1;
+`
+
+const FolderAddList = styled.ScrollView`
+  ${backgroundWithColor('gray_1')}
   flex: 1;
 `
 
@@ -33,6 +38,10 @@ const FolderAdd = () => {
   const [folderColor, setFolderColor] = useState<string>('navy')
   const [, fetchFolders] = useFolderList()
   const navigation = useNavigation()
+
+  const isCreatable = useMemo(() => {
+    return folderTitle.trim().length > 0 && folderColor
+  }, [folderColor, folderTitle])
 
   const onChangeText = useCallback((text: string) => {
     setFolderTitle(text)
@@ -55,19 +64,23 @@ const FolderAdd = () => {
   return (
     <FolderAddView>
       <Header>폴더 생성하기</Header>
-      <FolderAddContent>
-        <SectionTitle title="폴더 명" />
-        <SectionContent>
-          <Input value={folderTitle} onChangeText={onChangeText} />
-        </SectionContent>
-        <SectionTitle title="폴더 컬러 선택" />
-        <SectionContent>
-          <FolderColorList onColorChange={setFolderColor} />
-        </SectionContent>
-      </FolderAddContent>
-      <BottomButton>
-        <Button onPress={onPress}>링크를 담을 폴더 생성하기 </Button>
-      </BottomButton>
+      <FolderAddList>
+        <FolderAddContent>
+          <SectionTitle title="폴더 명" />
+          <SectionContent>
+            <Input value={folderTitle} onChangeText={onChangeText} />
+          </SectionContent>
+          <SectionTitle title="폴더 컬러 선택" />
+          <SectionContent>
+            <FolderColorList onColorChange={setFolderColor} />
+          </SectionContent>
+        </FolderAddContent>
+        <BottomButton>
+          <Button disabled={!isCreatable} onPress={onPress}>
+            링크를 담을 폴더 생성하기
+          </Button>
+        </BottomButton>
+      </FolderAddList>
     </FolderAddView>
   )
 }

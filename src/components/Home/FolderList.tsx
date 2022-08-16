8745a7c72421fbd59dece61984af3c777f4b10b7
esllo <master@esllo.com>
2022-08-17 03:41:39 +0900

    [#82] refactor: add margin to folder_list

--- src/components/Home/FolderList.tsx
+++ src/components/Home/FolderList.tsx
@@ -5,10 +5,11 @@ import FolderItem from './FolderItem'
 import useFolderList from './FolderList.hook'
 
 const FolderListScrollView = styled.ScrollView`
-  margin: 32px 0;
+  margin: 24px 0 0;
 `
 
 const FolderListView = styled.View`
+  margin: 8px 0 32px;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-between;

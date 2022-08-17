d21954774b93cfcc0a7c5dc4194cdb161a75fbe0
Seokhwan Park <master@esllo.com>
2022-08-18 06:21:17 +0900

    [#99] 리마인딩 알림 (#100)
    * [#99] chore: update image assets
    
    * [#99] feat: add hide_back to header
    
    * [#99] rename: folder_empty to empty
    
    * [#99] refactor: folder_empty to empty
    
    * [#99] feat: add reminding_notice components
    
    * [#99] feat: add reminding_notice page
    
    * [#99] feat: add card components

---
+++ src/components/Common/Card.tsx
@@ -0,0 +1,156 @@
+import React from 'react'
+import styled from '@emotion/native'
+import { ImageSourcePropType } from 'react-native'
+import { backgroundWithColor, shadow } from '../../styles/backgrounds'
+import { ColorPalette, Typo } from '../../styles/variable'
+import { fontWithColor } from '../../styles/fonts'
+import { flexWithAlign } from '../../styles/flexbox'
+import SVG from '../../assets/images/svg'
+
+const defaultSource = { uri: 'https://via.placeholder.com/1200x630' }
+
+const CardView = styled.View`
+  align-self: stretch;
+  align-items: stretch;
+  border-radius: 8px;
+  overflow: hidden;
+`
+
+const CardCoverImage = styled.Image`
+  height: 140px;
+`
+
+const CardCoverOverlay = styled.View`
+  background: rgba(0, 0, 0, 0.2);
+  height: 140px;
+  position: absolute;
+  top: 0;
+`
+
+const CardContent = styled.View`
+  ${backgroundWithColor('White')}
+  padding: 10px 16px 16px;
+`
+
+const CardTitleText = styled.Text`
+  ${Typo.Heading3_600}
+  ${fontWithColor('BlueGray_5')}
+`
+
+const CardDescriptionText = styled.Text`
+  ${Typo.Detail2_400}
+  ${fontWithColor('BlueGray_4')}
+  margin-top: 10px;
+`
+
+const CardTagList = styled.View`
+  ${flexWithAlign('center', 'flex-start', 'row')}
+  flex-wrap: wrap;
+  overflow: hidden;
+  height: 25px;
+  margin-top: 16px;
+`
+
+const CardTag = styled.Text`
+  ${backgroundWithColor('LinkkleBlueGray')}
+  ${Typo.Detail1_400}
+  ${fontWithColor('White')}
+  border-radius: 30px;
+  height: 25px;
+  margin-right: 9px;
+  padding: 0 8px;
+  line-height: 25px;
+  max-width: 100px;
+`
+
+const CardFavicon = styled.Image`
+  width: 28px;
+  height: 28px;
+  position: absolute;
+  left: 16px;
+  top: 16px;
+  border-radius: 14px;
+  overflow: hidden;
+  border: 2px solid ${ColorPalette.White};
+`
+
+const BookmarkTouchable = styled.TouchableOpacity`
+  width: 24px;
+  height: 24px;
+  right: 16px;
+  top: 16px;
+  position: absolute;
+`
+
+const MemoTouchable = styled.TouchableOpacity`
+  width: 24px;
+  height: 24px;
+  right: 56px;
+  top: 16px;
+  position: absolute;
+`
+
+const iconButtonInsets = { top: 8, bottom: 8, left: 8, right: 8 }
+
+interface Props {
+  source?: ImageSourcePropType
+  title: string
+  description?: string
+  tags?: string[]
+  favicon?: ImageSourcePropType
+  bookmark?: boolean
+  bookmarked?: boolean
+  memo?: boolean
+}
+
+const Card = ({
+  source,
+  title,
+  description,
+  tags,
+  favicon,
+  bookmark,
+  bookmarked,
+  memo
+}: Props) => {
+  return (
+    <CardView style={shadow}>
+      <CardCoverImage source={source || defaultSource} resizeMode="cover" />
+      <CardCoverOverlay />
+      {favicon && <CardFavicon source={favicon} resizeMode="contain" />}
+      {bookmark && (
+        <BookmarkTouchable hitSlop={iconButtonInsets}>
+          {bookmarked ? (
+            <SVG.BookmarkFilled stroke={ColorPalette.White} />
+          ) : (
+            <SVG.Bookmark stroke={ColorPalette.White} />
+          )}
+        </BookmarkTouchable>
+      )}
+      {memo && (
+        <MemoTouchable hitSlop={iconButtonInsets}>
+          <SVG.Memo stroke={ColorPalette.White} />
+        </MemoTouchable>
+      )}
+      <CardContent>
+        <CardTitleText numberOfLines={1}>{title}</CardTitleText>
+        {description && (
+          <CardDescriptionText numberOfLines={1}>
+            {description}
+          </CardDescriptionText>
+        )}
+        {tags && (
+          <CardTagList>
+            {tags.map(tag => (
+              <CardTag key={tag} numberOfLines={1}>
+                {tag}
+              </CardTag>
+            ))}
+          </CardTagList>
+        )}
+      </CardContent>
+    </CardView>
+  )
+}
+
+export default Card
---
+++ src/components/RemindingNotice/NoticeTop.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from '@emotion/native'
+import { backgroundWithColor } from '../../styles/backgrounds'
+import { Typo } from '../../styles/variable'
+import { fontWithColor } from '../../styles/fonts'
+
+const NoticeTopView = styled.View`
+  ${backgroundWithColor('White')}
+  justify-content:space-between;
+  height: 134px;
+  padding: 28px 24px 24px;
+`
+
+const NoticeFrameImage = styled.Image`
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 414px;
+  height: 133px;
+`
+
+const NoticeDateText = styled.Text`
+  ${Typo.Heading4_600}
+  ${fontWithColor('BlueGray_3')}
+`
+
+const NoticeTimeText = styled.Text`
+  ${Typo.Alarm2_600}
+  ${fontWithColor('BlueGray_5')}
+`
+
+const NoticeTop = () => {
+  return (
+    <NoticeTopView>
+      <NoticeFrameImage
+        source={require('../../assets/images/reminding_notice_frame.png')}
+        resizeMode="contain"
+      />
+      <NoticeDateText>2022년 08월 16일 (화)</NoticeDateText>
+      <NoticeTimeText>AM 09:30</NoticeTimeText>
+    </NoticeTopView>
+  )
+}
+
+export default NoticeTop

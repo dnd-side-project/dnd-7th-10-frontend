f19f84f5fada14823433d097f8b3e00839a60743
esllo <master@esllo.com>
2022-08-21 19:38:07 +0900

    [#108] feat: add firebase messaging subscription

--- App.tsx
+++ App.tsx
@@ -1,15 +1,32 @@
 import styled from '@emotion/native'
 import { NavigationContainer } from '@react-navigation/native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { RecoilRoot } from 'recoil'
 import Router from './src/pages/Router'
+import messaging from '@react-native-firebase/messaging'
+import { Alert, AppRegistry } from 'react-native'
 
 const SafeArea = styled.SafeAreaView`
   background: white;
   flex: 1;
 `
 
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in background', remoteMessage)
+})
+
 const App = () => {
+  useEffect(() => {
+    const messagingInstance = messaging()
+    const unsubscribe = messagingInstance.onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage))
+      console.warn('warn')
+      console.log(remoteMessage)
+    })
+
+    return unsubscribe
+  }, [])
+
   return (
     <SafeArea>
       <RecoilRoot>
@@ -20,5 +37,7 @@ const App = () => {
     </SafeArea>
   )
 }
+
+AppRegistry.registerComponent('app', () => App)
 
 export default App

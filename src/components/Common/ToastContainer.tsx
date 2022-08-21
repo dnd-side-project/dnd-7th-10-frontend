1a479881fcd2905e32b041523bc607af0dcc68ed
esllo <master@esllo.com>
2022-08-22 02:51:07 +0900

    [#112] feat: add toast, toast-container

---
+++ src/components/Common/ToastContainer.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from '@emotion/native'
+import { useRecoilValue } from 'recoil'
+import { toastsAtom } from '../../recoil/global'
+import Toast from './Toast'
+
+const ToastContainerView = styled.View`
+  position: absolute;
+  left: 24px;
+  right: 24px;
+  bottom: 0;
+`
+
+const ToastContainer = () => {
+  const toasts = useRecoilValue(toastsAtom)
+
+  return (
+    <ToastContainerView>
+      {(toasts || []).map(toast => (
+        <Toast
+          key={toast.id || toast.message}
+          warn={toast.warn}
+          check={toast.check}
+          variant={toast.variant}
+          offset={toast.offset}
+        >
+          {toast.message}
+        </Toast>
+      ))}
+    </ToastContainerView>
+  )
+}
+
+export default ToastContainer

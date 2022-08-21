10bbac1bea05797f875ebce7f3be63b1195bfe53
esllo <master@esllo.com>
2022-08-22 02:08:16 +0900

    [#110] refactor: change useApi to api

--- src/hooks/useTagList.ts
+++ src/hooks/useTagList.ts
@@ -1,19 +1,30 @@
+import { useCallback, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import api from '../lib/api'
 import { ITag, tagsAtom } from '../recoil/tags'
-import useRecoilApi from './useRecoilApi'
 
-export default function useTagList() {
-  const { isLoading, isError, recoilValue, refresh } = useRecoilApi<ITag[]>(
-    '/tag',
-    tagsAtom,
-    {
-      queryOnInitial: true
+export default function useTagList(): [boolean, ITag[], () => void] {
+  const [isTagLoading, setIsTagLoading] = useState<boolean>(false)
+  const [tags, setTags] = useRecoilState<ITag[]>(tagsAtom)
+
+  const fetchTagList = useCallback(() => {
+    if (isTagLoading) {
+      return
     }
-  )
 
-  return {
-    isTagError: isError,
-    isTagLoading: isLoading,
-    tags: recoilValue,
-    refreshTag: refresh
-  }
+    setIsTagLoading(true)
+    api
+      .get<ITag[]>('/tag')
+      .then(response => {
+        if (response.status === 200) {
+          setTags(response.data)
+        }
+      })
+      .catch(error => console.error(error))
+      .finally(() => {
+        setIsTagLoading(false)
+      })
+  }, [isTagLoading, setTags])
+
+  return [isTagLoading, tags, fetchTagList]
 }

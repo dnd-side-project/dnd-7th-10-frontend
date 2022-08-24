af8d1068ccdaea4eda2d8db558846e35e0758337
esllo <master@esllo.com>
2022-08-24 13:58:07 +0900

    [#122] feat: add favicon to card

--- src/components/FolderContent/FolderCard.tsx
+++ src/components/FolderContent/FolderCard.tsx
@@ -1,4 +1,5 @@
-import React, { useMemo } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import { ImageSourcePropType } from 'react-native'
 import { IArticle } from '../../recoil/folders'
 import Card from '../Common/Card'
 
@@ -7,12 +8,33 @@ interface Props {
 }
 
 const FolderCard = ({ article }: Props) => {
+  const [favicon, setFavicon] = useState<ImageSourcePropType | undefined>(
+    undefined
+  )
+
   const linkImage = useMemo(() => {
     if (article.openGraph.linkImage.startsWith('//')) {
       return `https:${article.openGraph.linkImage}`
     }
     return article.openGraph.linkImage || 'https://via.placeholder.com/1200x630'
   }, [article])
+
+  const getFaviconUrl = useCallback(() => {
+    const [protocol, url] = article.linkUrl.split('://')
+    const [host] = url.split('/')
+    return `${protocol}://${host}/favicon.ico`
+  }, [article.linkUrl])
+
+  useEffect(() => {
+    const uri = getFaviconUrl()
+    fetch(uri).then(({ status }) => {
+      if (status === 200) {
+        setFavicon({
+          uri: uri
+        })
+      }
+    })
+  }, [getFaviconUrl])
 
   const tags = useMemo(() => {
     return article.tags.map(({ tagName }) => tagName)
@@ -22,10 +44,14 @@ const FolderCard = ({ article }: Props) => {
     <Card
       title={article.openGraph.linkTitle}
       description={article.openGraph.linkDescription}
+      favicon={favicon}
       source={{
         uri: linkImage
       }}
       tags={tags}
+      bookmark
+      bookmarked={article.bookmark}
+      memo={article.memos && article.memos.length > 0}
     />
   )
 }

9a98fc0d4ef81ec097b41ac3c2e02e081009d7b0
esllo <master@esllo.com>
2022-08-24 17:00:04 +0900

    [#124] fix: wrong url check statement

--- src/lib/urlcheck.ts
+++ src/lib/urlcheck.ts
@@ -1,6 +1,7 @@
 export function isValidUrl(url: string) {
   if (
     url.includes('\n') ||
+    url.trim() === '' ||
     !url.match(
       /(https?)?(:\/\/)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g
     )
@@ -8,14 +9,7 @@ export function isValidUrl(url: string) {
     return false
   }
   if (!url.includes('://')) {
-    url = `https://${url}`
+    return `https://${url}`
   }
-  try {
-    if (new URL(url)) {
-      return url
-    }
-  } catch (e) {
-    return false
-  }
-  return false
+  return url
 }
